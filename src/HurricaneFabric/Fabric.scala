3ba66e5d9f6b93ae174cc9aafa58fdfd0e08938a
Palmer Dabbelt <palmer.dabbelt@eecs.berkeley.edu>
2014-11-28 10:42:36 -0800

    Re-indent the control portion
    I originally tried to have this within a scope block, but I guess
    Chisel's naming falls apart when I do this...  I'll eventually end up
    with this in another module, so I guess it doesn't matter all that
    much...

--- src/HurricaneFabric/Fabric.scala
+++ src/HurricaneFabric/Fabric.scala
@@ -38,77 +38,77 @@ class Fabric extends Module with FabricParameters {
   // A state machine for the control -- note that this is currently
   // _very_ simple in that it only allows one request/response pair in
   // the system at a time.
-    val host_req :: tile_req :: tile_resp :: host_resp :: Nil = Enum(UInt(), 4)
-    val state = Reg(init = host_req)
+  val host_req :: tile_req :: tile_resp :: host_resp :: Nil = Enum(UInt(), 4)
+  val state = Reg(init = host_req)
 
-    val sent      = Vec.fill(tile_count){ Reg(init = Bool(false)) }
-    val responded = Vec.fill(tile_count){ Reg(init = Bool(false)) }
+  val sent      = Vec.fill(tile_count){ Reg(init = Bool(false)) }
+  val responded = Vec.fill(tile_count){ Reg(init = Bool(false)) }
 
-    val request  = Reg(new ControlRequest)
-    val response = Vec.fill(tile_count){ Reg(new ControlResponse) }
+  val request  = Reg(new ControlRequest)
+  val response = Vec.fill(tile_count){ Reg(new ControlResponse) }
 
-    // By default don't allow anything to pass through at all
-    io.control_req .ready := Bool(false)
-    io.control_resp.valid := Bool(false)
-    io.control_resp.bits.data := response.map{r => r.data}.fold(Bits(0)){ _ | _ }
-    io.tiles.foreach{ tile =>
-      tile.control_req.valid  := Bool(false)
-      tile.control_req.bits   := request
-      tile.control_resp.ready := Bool(false)
-    }
+  // By default don't allow anything to pass through at all
+  io.control_req .ready := Bool(false)
+  io.control_resp.valid := Bool(false)
+  io.control_resp.bits.data := response.map{r => r.data}.fold(Bits(0)){ _ | _ }
+  io.tiles.foreach{ tile =>
+    tile.control_req.valid  := Bool(false)
+    tile.control_req.bits   := request
+    tile.control_resp.ready := Bool(false)
+  }
 
-    switch (state) {
-      is (host_req) {
-        io.control_req.ready := Bool(true)
+  switch (state) {
+    is (host_req) {
+      io.control_req.ready := Bool(true)
 
-        request := io.control_req.bits
+      request := io.control_req.bits
 
-        sent      := UInt(0)
-        responded := UInt(0)
+      sent      := UInt(0)
+      responded := UInt(0)
 
-        when (io.control_req.valid === Bool(true)) {
-          state := tile_req
-        }
-      }
-
-      is (tile_req) {
-        // Upon entrance to this state, the "sent" register has been
-        // set to TRUE for every tile that has successfully had a
-        // control request sent to it, and FALSE for every other tile.
-        // This attempts to enqueue a request for every sent-FALSE
-        // tile and then sets the sent bit when enqueueing a request.
-        (sent, io.tiles).zipped.map{ (sent, tile) =>
-          tile.control_req.valid := !sent
-          when (tile.control_req.ready === Bool(true)) {
-            sent := Bool(true)
-          }
-        }
-
-        when (sent.fold(Bool(true)){_ && _} === Bool(true)) {
-          state := tile_resp
-        }
-      }
-
-      is (tile_resp) {
-        (responded, io.tiles, response).zipped.map{ (responded, tile, response) =>
-          tile.control_resp.ready := !responded
-          when (tile.control_resp.valid === Bool(true)) {
-            responded := Bool(true)
-            response  := tile.control_resp.bits
-          }
-        }
-
-        when (responded.fold(Bool(true)){_ && _} === Bool(true)) {
-          state := host_resp
-        }
-      }
-
-      is (host_resp) {
-        io.control_resp.valid := Bool(true)
-
-        when (io.control_resp.ready === Bool(true)) {
-          state := host_req
-        }
+      when (io.control_req.valid === Bool(true)) {
+        state := tile_req
       }
     }
+
+    is (tile_req) {
+      // Upon entrance to this state, the "sent" register has been
+      // set to TRUE for every tile that has successfully had a
+      // control request sent to it, and FALSE for every other tile.
+      // This attempts to enqueue a request for every sent-FALSE
+      // tile and then sets the sent bit when enqueueing a request.
+      (sent, io.tiles).zipped.map{ (sent, tile) =>
+        tile.control_req.valid := !sent
+        when (tile.control_req.ready === Bool(true)) {
+          sent := Bool(true)
+        }
+      }
+
+      when (sent.fold(Bool(true)){_ && _} === Bool(true)) {
+        state := tile_resp
+      }
+    }
+
+    is (tile_resp) {
+      (responded, io.tiles, response).zipped.map{ (responded, tile, response) =>
+        tile.control_resp.ready := !responded
+        when (tile.control_resp.valid === Bool(true)) {
+          responded := Bool(true)
+          response  := tile.control_resp.bits
+        }
+      }
+
+      when (responded.fold(Bool(true)){_ && _} === Bool(true)) {
+        state := host_resp
+      }
+    }
+
+    is (host_resp) {
+      io.control_resp.valid := Bool(true)
+
+      when (io.control_resp.ready === Bool(true)) {
+        state := host_req
+      }
+    }
+  }
 }
